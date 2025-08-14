084290fedb810f9dc325fa50ffe7feb670d68f46
Mphammed Waleed <m.elsherbeny100@gmail.com>
2025-08-14 14:01:16 +0300

    Test Api With Mockito Part 2

--- lib/test_api_album/models/api_album.dart
+++ lib/test_api_album/models/api_album.dart
@@ -1,24 +1,25 @@
 import 'package:dio/dio.dart';
 
-  Future<AlbumModel> fetchAlbum({required Dio dio}) async {
-    Response response = await dio.get('https://jsonplaceholder.typicode.com/albums/1');
+Future<AlbumModel> fetchAlbum({required Dio dio}) async {
+  try {
+    Response response = await dio.get(
+      'https://jsonplaceholder.typicode.com/albums/1',
+    );
     if (response.statusCode == 200) {
       return AlbumModel.fromJson(response.data);
     } else {
       throw Exception('Failed to load album');
     }
+  } catch (e) {
+    throw Exception("Failed to load album $e");
   }
-
+}
 
 class AlbumModel {
   final int userId;
   final int id;
   final String title;
-  AlbumModel({
-    required this.userId,
-    required this.id,
-    required this.title,
-  });
+  AlbumModel({required this.userId, required this.id, required this.title});
 
   factory AlbumModel.fromJson(Map<String, dynamic> json) {
     return AlbumModel(
--- test/api_album_test.dart
+++ test/api_album_test.dart
@@ -1,7 +1,29 @@
-// import 'package:dio/dio.dart';
-// import 'package:mockito/annotations.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:unit_testing_in_flutter/test_api_album/models/api_album.dart';
 
-// @GenerateNiceMocks([Dio dio])
-// void main(){
+import 'api_album_test.mocks.dart';
 
-// }
+@GenerateMocks([Dio])
+void main() {
+  late MockDio mockDio;
+  setUp(() {
+    mockDio = MockDio();
+  });
+  test("Test fetchAlbum() Response", () async {
+    when(
+      mockDio.get("https://jsonplaceholder.typicode.com/albums/1"),
+    ).thenAnswer((_) async {
+      return Response(
+        requestOptions: RequestOptions(
+          path: "https://jsonplaceholder.typicode.com/albums/1",
+          data: {"userId": 1, "id": 1, "title": "quidem molestiae enim"},
+        ),
+        statusCode: 200,
+      );
+    });
+    expect(await fetchAlbum(dio: mockDio), isA<AlbumModel>());
+  });
+}
