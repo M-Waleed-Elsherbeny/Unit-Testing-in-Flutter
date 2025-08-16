75fb2f0102eac044ebef042bba77d614447636e8
Mphammed Waleed <m.elsherbeny100@gmail.com>
2025-08-16 09:14:13 +0300

    Finish Test Api With Mockito

--- test/api_album_test.dart
+++ test/api_album_test.dart
@@ -1,29 +1,67 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
-import 'package:mockito/mockito.dart';
+import 'package:mockito/mockito.dart' as mock;
 import 'package:unit_testing_in_flutter/test_api_album/models/api_album.dart';
 
 import 'api_album_test.mocks.dart';
 
 @GenerateMocks([Dio])
 void main() {
-  late MockDio mockDio;
-  setUp(() {
-    mockDio = MockDio();
-  });
-  test("Test fetchAlbum() Response", () async {
-    when(
-      mockDio.get("https://jsonplaceholder.typicode.com/albums/1"),
-    ).thenAnswer((_) async {
-      return Response(
-        requestOptions: RequestOptions(
-          path: "https://jsonplaceholder.typicode.com/albums/1",
-          data: {"userId": 1, "id": 1, "title": "quidem molestiae enim"},
-        ),
-        statusCode: 200,
+  group("Fetch Album Test", () {
+    late MockDio mockDio;
+    setUp(() { // Registers a function to be run before tests
+      mockDio = MockDio();
+    });
+    // tearDown(body) ==> Registers a function to be run after tests.
+    test("Test fetchAlbum() Response", () async {
+      mock
+          .when(mockDio.get("https://jsonplaceholder.typicode.com/albums/1"))
+          .thenAnswer((_) async {
+            return Response(
+              requestOptions: RequestOptions(
+                path: "https://jsonplaceholder.typicode.com/albums/1",
+              ),
+              statusCode: 200,
+              data: {"userId": 1, "id": 1, "title": "quidem molestiae enim"},
+            );
+          });
+      expect(await fetchAlbum(dio: mockDio), isA<AlbumModel>());
+    });
+    test("Exception throw request with an error", () {
+      mock
+          .when(mockDio.get("https://jsonplaceholder.typicode.com/albums/1"))
+          .thenAnswer((_) async {
+            return Response(
+              requestOptions: RequestOptions(
+                path: "https://jsonplaceholder.typicode.com/albums/1",
+              ),
+              statusCode: 404,
+              data: {"MsgError": "Page Not Found"},
+            );
+          });
+      expect(fetchAlbum(dio: mockDio), throwsException); // Matcher
+    });
+    test("Check Validate", () async {
+      mock
+          .when(mockDio.get("https://jsonplaceholder.typicode.com/albums/1"))
+          .thenAnswer((_) async {
+            return Response(
+              requestOptions: RequestOptions(
+                path: "https://jsonplaceholder.typicode.com/albums/1",
+              ),
+              statusCode: 200,
+              data: {"userId": 1, "id": 3, "title": "Fake Data"},
+            );
+          });
+      expect(
+        await fetchAlbum(dio: mockDio),
+        isA<AlbumModel>().having(
+          (album) => album.title,
+          "Check Title",
+          "Fake Data",
+        ).having((album) => album.id, "Check Id", 3),
       );
     });
-    expect(await fetchAlbum(dio: mockDio), isA<AlbumModel>());
   });
 }
